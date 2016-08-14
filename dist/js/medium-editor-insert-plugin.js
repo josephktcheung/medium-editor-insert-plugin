78caf4f35be07bba622309e720e46310a8fec9f6
josephktcheung <josephktcheung@gmail.com>
2016-08-14 11:52:49 +0800

    Fix css

--- dist/js/medium-editor-insert-plugin.js
+++ dist/js/medium-editor-insert-plugin.js
@@ -106,15 +106,15 @@ this["MediumInsert"]["Templates"]["src/js/templates/embeds-wrapper.hbs"] = Handl
 },"useData":true});
 
 this["MediumInsert"]["Templates"]["src/js/templates/gallery-modal.hbs"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
-    return "                <figure><img class=\"medium-insert-gallery-image\" src=\""
+    return "                    <figure><img class=\"medium-insert-gallery-image\" src=\""
     + container.escapeExpression(container.lambda(depth0, depth0))
     + "\" alt=\"\"></figure>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div class=\"medium-insert-gallery-modal\">\n    <div class=\"medium-insert-gallery-modal-guts\">\n        <h1>Select images to insert</h1>\n        <div class=\"medium-insert-gallery-images medium-insert-images medium-insert-images-grid\">\n"
+  return "<div class=\"medium-insert-gallery-modal\">\n    <div class=\"medium-insert-gallery-actions\">\n        <button id=\"medium-insert-gallery-cancel\">Cancel</button>\n        <button id=\"medium-insert-gallery-confirm\">Confirm</button>\n    </div>\n    <div class=\"medium-insert-gallery-modal-guts\">\n        <h4>Select images to insert</h4>\n        <div class=\"medium-insert-gallery-modal-content\">\n            <div class=\"medium-insert-gallery-images medium-insert-images medium-insert-images-grid\">\n"
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.images : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "        </div>\n        <div class=\"medium-insert-gallery-actions\">\n            <button id=\"medium-insert-gallery-cancel\">Cancel</button>\n            <button id=\"medium-insert-gallery-confirm\">Confirm</button>\n        </div>\n    </div>\n</div>";
+    + "            </div>\n        </div>\n    </div>\n</div>";
 },"useData":true});
 
 this["MediumInsert"]["Templates"]["src/js/templates/images-fileupload.hbs"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
